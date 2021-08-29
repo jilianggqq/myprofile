d930aedb75c26edfe21f482507203e8eef31ca9f
Qiqiang Guan <guan5678xyz@gmail.com>
2021-08-29 00:43:02 -0700

    test

--- index.js
+++ index.js
@@ -60,6 +60,6 @@ router.get('/mf',function(req,res){
 
 //add the router
 app.use('/', router);
-app.listen(process.env.port || 3000);
+app.listen(process.env.port || 5000);
 
-console.log('Running at Port 3000');
+console.log('Running at Port 5000');
