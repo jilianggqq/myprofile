0117e87ddafc940f49a2f9f01825ca9c84a78319
Qiqiang Guan <guan5678xyz@gmail.com>
2021-08-29 00:55:43 -0700

    tes

--- index.js
+++ index.js
@@ -1,65 +1,64 @@
-const express = require('express');
+// import express from 'express'
+// using as Reactjs 2015 modules
+const express = require("express");
+
+// generate a new application running as apps
 const app = express();
-const path = require('path');
+const path = require("path");
 const router = express.Router();
 
-app.use(express.static(path.join(__dirname, 'public')))
+// route finalhandler
+app.get("/", (req, res) => {
+  res.send({
+    Hello: "world"
+  });
+});
 
-router.get('/',function(req,res){
-  res.sendFile(path.join(__dirname+'/dist/index.html'));
+console.log(__dirname);
+
+router.get("/v1/login", function (req, res) {
+  res.sendFile(path.join(__dirname + "/pages/index.html"));
   //__dirname : It will resolve to your project folder.
 });
 
-router.get('/index',function(req,res){
-  res.sendFile(path.join(__dirname+'/dist/index.html'));
+router.get("/v1/dashboard", function (req, res) {
+  res.sendFile(path.join(__dirname + "/pages/dashboard.html"));
   //__dirname : It will resolve to your project folder.
 });
 
-router.get('/ob',function(req,res){
-  res.sendFile(path.join(__dirname+'/dist/ob.html'));
+router.get("/v1/product", function (req, res) {
+  res.sendFile(path.join(__dirname + "/pages/product.html"));
   //__dirname : It will resolve to your project folder.
 });
 
-router.get('/vsp',function(req,res){
-  res.sendFile(path.join(__dirname+'/dist/vsp.html'));
+router.get("/v1/order", function (req, res) {
+  res.sendFile(path.join(__dirname + "/pages/order.html"));
   //__dirname : It will resolve to your project folder.
 });
 
-router.get('/ma',function(req,res){
-  res.sendFile(path.join(__dirname+'/dist/ma.html'));
+router.get("/v1/customer", function (req, res) {
+  res.sendFile(path.join(__dirname + "/pages/customer.html"));
   //__dirname : It will resolve to your project folder.
 });
 
-router.get('/me',function(req,res){
-  res.sendFile(path.join(__dirname+'/dist/me.html'));
-  //__dirname : It will resolve to your project folder.
-});
-
-router.get('/mis',function(req,res){
-  res.sendFile(path.join(__dirname+'/dist/mis.html'));
-  //__dirname : It will resolve to your project folder.
-});
-
-router.get('/sm',function(req,res){
-  res.sendFile(path.join(__dirname+'/dist/sm.html'));
-  //__dirname : It will resolve to your project folder.
-});
-
-router.get('/mf',function(req,res){
-  res.sendFile(path.join(__dirname+'/dist/mf.html'));
-  //__dirname : It will resolve to your project folder.
-});
-
-// router.get('/about',function(req,res){
-//   res.sendFile(path.join(__dirname+'/about.html'));
+// router.get("/about", function (req, res) {
+//   res.sendFile(path.join(__dirname + "/about.html"));
 // });
 
-// router.get('/sitemap',function(req,res){
-//   res.sendFile(path.join(__dirname+'/sitemap.html'));
+// router.get("/sitemap", function (req, res) {
+//   res.sendFile(path.join(__dirname + "/sitemap.html"));
 // });
 
 //add the router
-app.use('/', router);
-app.listen(process.env.port || 5000);
+app.use("/", router);
 
-console.log('Running at Port 5000');
+// the running time underlying environment where node js runs on top of.
+// if the app is running in local, process.env.PORT === undefined.
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT);
+
+// app.use(express.static(__dirname + "/public"));
+app.use('/static', express.static('public'))
+
+console.log("app is running in poat " + PORT);
